1b38a299f5ba81d1ee01b62a49476c4c50f8057c
marekpridal <m.pridal29@gmail.com>
2016-04-27 21:23:10 +0200

    První finální verze univerzální aplikace pro Windows 10 a Windows 10 Mobile

--- BMI/MainPage.xaml.cs
+++ BMI/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace BMI
@@ -25,6 +26,54 @@ namespace BMI
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            float vaha, vyska, vypocet;
+
+            if((float.TryParse(textBox1.Text, out vaha))&&(float.TryParse(textBox2.Text,out vyska)))
+            {
+                vyska = vyska / 100;
+                vypocet = ((vaha) / (vyska*vyska));
+                if(vypocet<=16.5)
+                {
+                    textBlock1.Foreground = new SolidColorBrush(Windows.UI.Colors.DeepSkyBlue);
+                    textBlock1.Text = "Váš BMI =" + vypocet.ToString() + ", trpíte těžkou podvýživou";
+                }else if(vypocet<=18.5)
+                {
+                    textBlock1.Foreground = new SolidColorBrush(Windows.UI.Colors.SkyBlue);
+                    textBlock1.Text = "Váš BMI =" + vypocet.ToString() + ", trpíte podvýživou";
+                }else if (vypocet <= 25)
+                {
+                    textBlock1.Foreground = new SolidColorBrush(Windows.UI.Colors.Green);
+                    textBlock1.Text = "Váš BMI = " + vypocet.ToString() + ", máte ideální váhu";
+                }
+                else if (vypocet <= 30)
+                {
+                    textBlock1.Foreground = new SolidColorBrush(Windows.UI.Colors.Yellow);
+                    textBlock1.Text = "Váš BMI =" + vypocet.ToString() + ", trpíte nadváhou";
+                }
+                else if (vypocet <= 35)
+                {
+                    textBlock1.Foreground = new SolidColorBrush(Windows.UI.Colors.Orange);
+                    textBlock1.Text = "Váš BMI =" + vypocet.ToString() + ", máte mírnou obezitu";
+                }
+                else if (vypocet <= 40)
+                {
+                    textBlock1.Foreground = new SolidColorBrush(Windows.UI.Colors.OrangeRed);
+                    textBlock1.Text = "Váš BMI =" + vypocet.ToString() + ", máte střední obezitu";
+                }
+                else if (vypocet > 40)
+                {
+                    textBlock1.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                    textBlock1.Text = "Váš BMI =" + vypocet.ToString() + ", trpíte morbidní obezitou";
+                }
+            }
+            else
+            {
+                //textBlock1.Text = "Zadej hodnoty";
+            }
         }
     }
 }
